7148a00bfd3acae9b496c077283d9ecdd27a85d5
Abhi1234cs21 <kamleshadajariya6252@gmail.com>
2025-03-06 15:46:47 +0530

    HTTP Method Added

---
+++ src/main/java/com/example/AdressBookApp/model/Contact.java
@@ -0,0 +1,21 @@
+package com.example.AdressBookApp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+}
